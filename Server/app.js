7d4ebb068a3bc45e9e57a0adfa1fbf89a3e14f8d
Cjoloug75 <connoroloughlin95@gmail.com>
2025-10-27 19:04:42 -0400

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -10,6 +10,61 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false}));
 
+app.post('/register', (request, response) => {
+    const {
+        username,
+        password,
+        firstname,
+        lastname,
+        salary,
+        age,
+    } = request.body;
+
+    const db = dbService.getDbServiceInstance();
+    
+    const result = db.registerNewUser({
+        username,
+        password,
+        firstname,
+        lastname,
+        salary,
+        age
+    });
+
+    result
+    .then(data => response.json({
+        success: true,
+        message: "User successfully registered.",
+        data: data
+    }))
+    .catch(err => {
+        console.log(err);
+            if (err.message && err.message.includes('Duplicate entry')) {
+                response.status(409).json({
+                    success: false,
+                    message: "A user with that username already exists."
+                });
+            }
+    });
+});
+app.post('/login', (request, response) => {
+    const { username, password } = request.body;
+    const db = dbService.getDbServiceInstance();
+    db.loginUser({ username, password })
+        .then(user => {
+            if (user && user.id) {
+                db.updateLastLogin(user.id);
+            }
+            response.json({ success: true, username: user.username });
+        })
+        .catch(error => {
+            console.log("Login Failure:", error.message);
+            response.status(401).json({
+                success: false,
+                message: "Invalid username or password."
+            });
+        });
+});
 //create
 app.post('/insert', (request, response) =>{
     const {name} =  request.body;
@@ -55,21 +110,106 @@ app.delete('/delete/:id', (request, response)=>{
 
 })
 
-app.get('/search/:name', (request, response) => { // we can debug by URL
+app.get('/users/by-username/:username', (request, response) => { 
     
-    const {name} = request.params;
+    const {username} = request.params;
     
-    console.log(name);
+    console.log(username);
 
     const db = dbService.getDbServiceInstance();
 
-    let result;
-    if(name === "all") // in case we want to search all
-        result = db.getAllData()
-    else
-       result =  db.searchByName(name); // call a DB function
+    db.searchByUserName(username)
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
+});
 
-    result
+app.get('/users/by-firstname/:firstname', (request, response) => { 
+    
+    const {firstname} = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    db.searchByFirstName(firstname)
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
+});
+
+app.get('/users/by-lastname/:lastname', (request, response) => {
+    
+    const {lastname} = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    db.searchByLastName(lastname)
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
+});
+
+app.get('/users/by-id/:id', (request, response) => {
+    
+    const {id} = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    db.searchById(id)
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
+});
+
+app.get('/users/by-salary-range', (request, response) => {
+    
+    const {min,max} = request.query;
+    if (!min || !max) {
+        return response.status(400).json({ success: false, message: "Missing required 'min' and 'max' parameters." });
+    }
+    const db = dbService.getDbServiceInstance();
+
+    db.searchBySalaryRange(min,max)
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
+});
+
+app.get('/users/by-age-range', (request, response) => {
+    
+    const {min, max} = request.query;
+    if (!min || !max) {
+        return response.status(400).json({ success: false, message: "Missing required 'min' and 'max' parameters." });
+    }
+    const db = dbService.getDbServiceInstance();
+
+    db.searchByAgeRange(min, max)
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
+});
+
+app.get('/users/registered/after/:id', (request, response) => {
+    
+    const {id} = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    db.searchRegisteredAfterUser(id)
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
+});
+
+app.get('/users/registered/same-day/:id', (request, response) => {
+    
+    const {id} = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    db.searchRegisteredSameDay(id)
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
+});
+
+app.get('/users/registered/today', (request, response) => {
+    const db = dbService.getDbServiceInstance();
+
+    db.searchRegisteredToday()
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
+});
+app.get('/users/never-signed-in', (request, response) => {
+    const db = dbService.getDbServiceInstance();
+
+    db.searchNeverSignedIn()
     .then(data => response.json({data: data}))
     .catch(err => console.log(err));
 });
